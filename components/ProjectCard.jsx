a7816d35949832ec89d7ff03bf9f834099dc3689
yeeway <yiwei.suuu@gmail.com>
2024-05-26 15:16:16 +0800

    refactor ProjectCard

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,7 +1,8 @@
+import Image from "next/image";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
-import { CursorArrowRaysIcon } from "@heroicons/react/24/solid";
+import { CursorArrowRaysIcon, ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
+import { ClockIcon } from "@heroicons/react/24/outline";
 import projectData from "@/data/projectData";
-import ProjectModalPage from "@/components/ProjectModalPage";
 
 export default function ProjectCard({number}) {
   const project = projectData.find((project) => project.number === number);
@@ -17,12 +18,40 @@ export default function ProjectCard({number}) {
             ? project.description.substring(0, 150) + "......read more"
             : project.description}
         </p>
-        <DialogTrigger className="absolute bottom-5 right-5 rounded-full border border-black px-3 py-1">
+        <DialogTrigger className="absolute bottom-5 right-5 rounded-full border border-black px-3 py-1 hover:bg-gray-300">
           詳細介紹
           <CursorArrowRaysIcon className=" inline size-6" />
         </DialogTrigger>
       </div>
-      <ProjectModalPage number={number} />
+      <DialogContent className="flex max-h-[90%] max-w-[80%] flex-col overflow-auto lg:flex-row">
+        <div className="lg:w-3/5">
+          <h3 className="text-lg font-bold text-Red">{project.team}</h3>
+          <h3 className="text-2xl font-bold">{project.name}</h3>
+          <p><ClockIcon className="mr-1 inline size-5" />發表時間：{project.demoTime}</p>
+          <p className="mt-4">{project.description}</p>
+          <div className="mt-4 flex flex-wrap gap-5 text-sm">
+            <div className="flex cursor-pointer items-center rounded-md border border-black p-2 ">
+              海報(待更新)
+              <ArrowTopRightOnSquareIcon className="ml-1 inline size-5" />
+            </div>
+            <div className="flex cursor-pointer items-center rounded-md border border-black p-2 ">
+              簡報(待更新)
+              <ArrowTopRightOnSquareIcon className="ml-1 inline size-5" />
+            </div>
+            <div className="flex cursor-pointer items-center rounded-md border border-black p-2 ">
+              Demo 影片(待更新)
+              <ArrowTopRightOnSquareIcon className="ml-1 inline size-5" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="my-5 bg-gray-300">
+            <Image src={project.membersPhotoLink} alt="組照" width={500} height={300} />
+          </div>
+          <div className="text-lg font-bold">團隊成員</div>
+          <span>{project.members}</span>
+        </div>
+      </DialogContent>
     </Dialog>
   );
 }
